3a0d574e77579472355e2a7002a161f2b44bb5cd
ahangchen <1418659400@qq.com>
2016-05-11 15:05:51 +0800

    to python 3

--- src/hello_world.py
+++ src/hello_world.py
@@ -3,4 +3,4 @@ features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print clf.predict([[150, 0]])
+print(clf.predict([[150, 0]]))
